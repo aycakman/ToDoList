56c515f41f29afb79a775795b02a7f29eba3b633
aycakman <aycaakman0@gmail.com>
2023-07-03 13:28:49 +0300

    App design completed

---
+++ ToDoList/ToDoList/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Ayca Akman on 3.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var addButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
